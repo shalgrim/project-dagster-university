fefafe8aa5ca9d0c9059541a272eea43aa4f19a4
Scott Halgrim <shalgrim@gmail.com>
2025-12-16 17:33:38 -0800

    add my own asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -8,7 +8,7 @@ from dagster_essentials.defs.assets import constants
 def taxi_trips_file() -> None:
     """
     raw parquet files for the taxi trips dataset.
-       Sourced from the NYC Open Data portal.
+    Sourced from the NYC Open Data portal.
     """
     month_to_fetch = "2023-03"
     raw_trips = requests.get(
@@ -19,3 +19,16 @@ def taxi_trips_file() -> None:
         constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
     ) as output_file:
         output_file.write(raw_trips.content)
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+    csv file with unique identifier and name for each part of NYC as a distinct taxi zone
+    Sourced from the NYC Open Data portal.
+    """
+    # TODO: set up my file watchers on this machine
+    raw_zones = requests.get("https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv")
+
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(raw_zones.content)
+
